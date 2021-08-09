3dffa548f501b5edd9d48dff097358de467920ea
Xccz0299@myoffice365.site <Xccz0299@myoffice365.site>
2021-08-09 09:58:53 +0300

    Implemented Bottom Bar (UI Fragments)

--- app/src/main/java/com/example/servopay/HomeActivity.java
+++ app/src/main/java/com/example/servopay/HomeActivity.java
@@ -1,14 +1,66 @@
 package com.example.servopay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.servopay.Fragments.HomeFragment;
+import com.example.servopay.Fragments.NotificationFragment;
+import com.example.servopay.Fragments.ProfileFragment;
+import com.example.servopay.Fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private Fragment selectorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation); //needs casting for some reason
+        //bottomNavigationView.findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home:
+                        selectorFragment= new HomeFragment();
+                        break;
+
+                    case R.id.nav_search:
+                        selectorFragment = new SearchFragment();
+                        break;
+
+                    case R.id.nav_add:
+                        selectorFragment = null;
+                        startActivity(new Intent(HomeActivity.this,PostActivity.class));
+                        break;
+
+                    case R.id.nav_heart:
+                        selectorFragment = new NotificationFragment();
+
+                    case R.id.nav_profile:
+                        selectorFragment= new ProfileFragment();
+                }
+                if(selectorFragment != null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectorFragment).commit();
+                }
+                return true;
+            }
+        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
     }
 }
