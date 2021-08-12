63ce27fc091f732731e116b717f2811243f8da9e
Xccz0299@myoffice365.site <Xccz0299@myoffice365.site>
2021-08-12 10:11:00 +0300

    Impelmented Likes fueture on UI + Database

---
+++ app/src/main/java/com/example/servopay/Adapter/PostAdapter.java
@@ -0,0 +1,168 @@
+package com.example.servopay.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.servopay.Model.Post;
+import com.example.servopay.Model.User;
+import com.example.servopay.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.hendraanggrian.appcompat.widget.SocialTextView;
+import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.Viewholder>{
+
+    private Context mContext;
+    private List<Post> mPosts;
+
+    private FirebaseUser firebaseUser;
+
+    public PostAdapter(Context mContext, List<Post> mPosts) {
+        this.mContext = mContext;
+        this.mPosts = mPosts;
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.post_item,parent,false);
+        return new PostAdapter.Viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull PostAdapter.Viewholder holder, int position) {
+        final Post post = mPosts.get(position);
+        Picasso.get().load(post.getImageurl()).into(holder.post_image);
+        holder.description.setText(post.getDescription());
+
+        FirebaseDatabase.getInstance().getReference().child("Users").child(post.getPublisher()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+
+                if(user.getImageurl().equals("default")){
+                    holder.imageProfile.setImageResource(R.mipmap.ic_launcher);
+                }
+                else{
+                    Picasso.get().load(user.getImageurl()).into(holder.imageProfile);
+                }
+                holder.username.setText(user.getUsername());
+                holder.author.setText(user.getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+        isLiked(post.getPostid(),holder.like);
+        noOfLikes(post.getPostid(), holder.noOfLikes);
+
+        holder.like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(holder.like.getTag().equals("like")){
+                    FirebaseDatabase.getInstance().getReference().child("Likes").child(post.getPostid()).child(firebaseUser.getUid()).setValue(true);
+                }
+                else{
+                    FirebaseDatabase.getInstance().getReference().child("Likes").child(post.getPostid()).child(firebaseUser.getUid()).removeValue();
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPosts.size();
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder{
+
+        public ImageView imageProfile,post_image,like,comment,save,more;
+
+        public TextView username,noOfLikes,author, noOfComments;
+
+        SocialTextView description;
+
+        public Viewholder(@NonNull @NotNull View itemView) {
+            super(itemView);
+
+            imageProfile = itemView.findViewById(R.id.profile_image);
+            post_image = itemView.findViewById(R.id.post_image);
+            like = itemView.findViewById(R.id.like);
+            comment = itemView.findViewById(R.id.comment);
+            save = itemView.findViewById(R.id.save);
+            more = itemView.findViewById(R.id.more);
+
+            username = itemView.findViewById(R.id.username);
+            noOfLikes = itemView.findViewById(R.id.likes_text);
+            author = itemView.findViewById(R.id.author);
+            noOfComments = itemView.findViewById(R.id.num_of_comments);
+            description = itemView.findViewById(R.id.description);
+
+
+
+        }
+    }
+
+    private void isLiked(String postId, ImageView imageView){
+        FirebaseDatabase.getInstance().getReference().child("Likes").child(postId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(firebaseUser.getUid()).exists()){
+                    imageView.setImageResource(R.drawable.ic_liked);
+                    imageView.setTag("liked");
+                }
+                else{
+                    imageView.setImageResource(R.drawable.ic_like);
+                    imageView.setTag("like");
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+
+    private void noOfLikes (String postID, TextView text){
+        FirebaseDatabase.getInstance().getReference().child("Likes").child(postID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot dataSnapshot) {
+                text.setText(dataSnapshot.getChildrenCount()+ "likes");
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/com/example/servopay/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/servopay/Fragments/HomeFragment.java
@@ -2,20 +2,106 @@ package com.example.servopay.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.servopay.Adapter.PostAdapter;
+import com.example.servopay.Model.Post;
 import com.example.servopay.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerViewPosts;
+    private PostAdapter postAdapter;
+    private List<Post> postList;
+
+    private List<String> followingList; //show only posts that the user follows
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view=inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerViewPosts=view.findViewById(R.id.recycler_view_posts);
+        recyclerViewPosts.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setStackFromEnd(true); //latest posts are available from the top
+        linearLayoutManager.setReverseLayout(true);
+        recyclerViewPosts.setLayoutManager(linearLayoutManager);
+        postList = new ArrayList<>();
+        postAdapter = new PostAdapter(getContext(),postList);
+        recyclerViewPosts.setAdapter(postAdapter);
+
+        followingList=new ArrayList<>();
+
+        checkFollowingUsers();
+
+
+        return view;
+    }
+
+    private void checkFollowingUsers() {
+
+        FirebaseDatabase.getInstance().getReference().child("Follow").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("following").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot dataSnapshot) {
+                followingList.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    followingList.add(snapshot.getKey());
+                }
+
+                readPosts();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void readPosts() {
+
+        FirebaseDatabase.getInstance().getReference().child("Posts").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot dataSnapshot) {
+                postList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Post post = snapshot.getValue(Post.class);
+
+                    for (String id : followingList){
+                        if (post.getPublisher().equals(id)){
+                            postList.add(post);
+                        }
+                    }
+                }
+                postAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
